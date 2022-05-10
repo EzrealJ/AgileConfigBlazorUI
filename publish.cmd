a9674670bd3b45bfa37ea1bd3fd244a7fee6df1d
Ezreal <memory_of_you@hotmail.com>
2022-05-11 00:40:53 +0800

    update docs

--- publish.cmd
+++ publish.cmd
@@ -2,3 +2,4 @@ set output=.\docs
 if exist "%output%" rd /S /Q "%output%"
 dotnet publish .\src\AgileConfig.BlazorUI\AgileConfig.BlazorUI.csproj -c Release -o %output%
 XCopy %output%\wwwroot %output% /E/H/C/I
+rd /S /Q %output%\wwwroot
