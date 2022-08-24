04be76be1b14164f0fc801ad192a3b8709848f9c
Ezreal <memory_of_you@hotmail.com>
2022-08-24 20:45:55 +0800

    try fix githubpages

--- src/AgileConfig.BlazorUI/Consts/RoutePath.cs
+++ src/AgileConfig.BlazorUI/Consts/RoutePath.cs
@@ -2,17 +2,17 @@
 {
     public class RoutePath
     {
-        public const string APP = "/app";
-        public const string CLIENT = "/client";
-        public const string HOME = "/home";
-        public const string INIT_PASSWORD = "/initpassword";
-        public const string LOG = "/log";
-        public const string LOGIN = "/login";
-        public const string LOGOUT = "/logout";
-        public const string NODE = "/node";
-        public const string RESET_PASSWORD = "/resetpassword";
-        public const string SERVICE = "/service";
-        public const string USER = "/user";
-        public const string AGILE_CONFIG_SERVER_MANAGER = "agileconfigservermanager";
+        public const string APP = "./app";
+        public const string CLIENT = "./client";
+        public const string HOME = "./home";
+        public const string INIT_PASSWORD = "./initpassword";
+        public const string LOG = "./log";
+        public const string LOGIN = "./login";
+        public const string LOGOUT = "./logout";
+        public const string NODE = "./node";
+        public const string RESET_PASSWORD = "./resetpassword";
+        public const string SERVICE = "./service";
+        public const string USER = "./user";
+        public const string AGILE_CONFIG_SERVER_MANAGER = "./agileconfigservermanager";
     }
 }
